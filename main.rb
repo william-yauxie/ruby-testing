02ebdea8fd24b1b9c9de8b48c94061f4c77ca2ea
William Yau Xie <william.yauxie@zendesk.com>
2022-08-15 02:24:45 +1000

    finished testing functions

--- main.rb
+++ main.rb
@@ -5,4 +5,34 @@ print("Password is: " + password)
 =end
 
 class Password 
+
+    def initialize(user_password)
+        @password = user_password
+    end
+
+    def get_password
+        return "#@password"
+    end
+
+    def is_palidrome?
+        # checks if palindrome
+        # racecar
+        # anna
+        count = @password.size
+        for i in 0..count - 1 do 
+ #          puts ("entered for loop")
+  #          puts (@password[i] + " and " + @password[count - 1 - i])
+            if @password[i] != @password[count - 1 - i]
+   #             puts (@password[i] + " and " + @password[count - 1 - i])
+   #             puts ("is not palindrome")
+                return false
+            end
+        end
+        return true
+    end
+
 end
+
+new_password = Password.new("asdf")
+puts(new_password.get_password)
+puts(new_password.is_palidrome?)
--- spec/spec_password.rb
+++ spec/spec_password.rb
@@ -1,9 +1,20 @@
 require "./main"
+
 describe Password do
+
     it "is at least 5 characters long" do
+        password = Password.new("12345")
+        expect(password.get_password.size).to be >= 5
     end
-    it "doesn't contain sandwich" do
+
+    it "contains sandwich" do
+        password = Password.new("wichsand")
+        expect("sandwich").not_to match(password.get_password)
     end
+
     it "isn't a palindrome" do
+        password = Password.new("asdfg")
+        expect(password.is_palidrome?).to be false
     end
+
 end
