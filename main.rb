ca5f3d681a0e26e6382ee018d67ee544d5a3486e
William Yau Xie <william.yauxie@zendesk.com>
2022-08-15 00:50:56 +1000

    made spec file, made password class

---
+++ main.rb
@@ -0,0 +1,8 @@
+=begin
+print("Enter password: ")
+password = gets
+print("Password is: " + password)
+=end
+
+class Password 
+end
---
+++ spec/spec_password.rb
@@ -0,0 +1,9 @@
+require "./main"
+describe Password do
+    it "is at least 5 characters long" do
+    end
+    it "doesn't contain sandwich" do
+    end
+    it "isn't a palindrome" do
+    end
+end
